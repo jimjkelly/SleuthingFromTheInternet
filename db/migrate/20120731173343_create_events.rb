4a71aeb0c5eb52d79b2b58f529453c64f19b9ae3
Jim Kelly <pthread1981@gmail.com>
2012-08-08 04:43:08 +0200

    fix bug with depth, switched to storing all items as strings, at least for now.  when I take the next step to normalize the values some may return to ints/floats

--- db/migrate/20120731173343_create_events.rb
+++ db/migrate/20120731173343_create_events.rb
@@ -1,13 +1,13 @@
 class CreateEvents < ActiveRecord::Migration
   def up
     create_table :events do |t|
-      t.float     :mag
+      t.string    :mag
       t.integer   :time
       t.string    :url
       t.string    :source
-      t.float     :latitude
-      t.float     :longitude
-      t.float     :depth
+      t.string    :latitude
+      t.string    :longitude
+      t.string    :depth
       t.integer   :retrieved
     end
   end
