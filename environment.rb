64f0bd554f3b09deb64718a6722e3b49f34d673b
Jim Kelly <pthread1981@gmail.com>
2012-11-06 19:18:33 +0100

    Add port per heroku staff recommendation

--- environment.rb
+++ environment.rb
@@ -31,6 +31,7 @@ configure :production do
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host     => db.host,
+    :port     => db.port,
     :username => db.user,
     :password => db.password,
     :database => db.path[1..-1],
